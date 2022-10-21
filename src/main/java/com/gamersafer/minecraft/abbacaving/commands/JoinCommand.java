c9e3d44210d423f41f317060319cec44e20be474
Draycia <lonelyyordle@gmail.com>
2022-10-20 18:35:58 -0700

    Allow random map selection in join command

--- src/main/java/com/gamersafer/minecraft/abbacaving/commands/JoinCommand.java
+++ src/main/java/com/gamersafer/minecraft/abbacaving/commands/JoinCommand.java
@@ -2,7 +2,9 @@ package com.gamersafer.minecraft.abbacaving.commands;
 
 import com.gamersafer.minecraft.abbacaving.AbbaCavingPlugin;
 import com.gamersafer.minecraft.abbacaving.lobby.LobbyQueue;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -20,19 +22,25 @@ public class JoinCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command, @NotNull final String label, @NotNull final String[] args) {
-        if (args.length == 0) {
-            this.plugin.message(sender, this.plugin.configMessage("join-invalid-map"));
-            return false;
+        final LobbyQueue queue;
+
+        if (args.length == 0 || args[0].equalsIgnoreCase("random")) {
+            final List<String> mapNames = this.plugin.configuredMapNames();
+            final String mapName = mapNames.get(ThreadLocalRandom.current().nextInt(mapNames.size()));
+
+            queue = this.plugin.lobby().lobbyQueue(mapName);
+            // TODO: don't try to put player into active games or full lobbies
+            //  add activeQueues() method to Lobby class and pick random from there.
+        } else {
+            final String input = args[0];
+
+            if (this.plugin.mapSettings(input) == null) {
+                this.plugin.message(sender, this.plugin.configMessage("join-invalid-map"));
+                return false;
+            }
+
+            queue = this.plugin.lobby().lobbyQueue(input);
         }
-
-        final String input = args[0];
-
-        if (this.plugin.mapSettings(input) == null) {
-            this.plugin.message(sender, this.plugin.configMessage("join-invalid-map"));
-            return false;
-        }
-
-        final LobbyQueue queue = this.plugin.lobby().lobbyQueue(input);
 
         final Player player;
 
@@ -58,7 +66,7 @@ public class JoinCommand implements CommandExecutor {
 
         queue.addPlayer(player.getUniqueId());
 
-        this.plugin.message(sender, this.plugin.configMessage("join-lobby"), Map.of("map", input));
+        this.plugin.message(sender, this.plugin.configMessage("join-lobby"), Map.of("map", queue.mapName()));
 
         return true;
     }
