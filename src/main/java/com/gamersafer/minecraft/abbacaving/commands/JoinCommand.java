0612e39e58edd440364cef91bb07590803538323
Draycia <lonelyyordle@gmail.com>
2022-10-20 18:31:12 -0700

    Allow console and admins to put players into queues

--- src/main/java/com/gamersafer/minecraft/abbacaving/commands/JoinCommand.java
+++ src/main/java/com/gamersafer/minecraft/abbacaving/commands/JoinCommand.java
@@ -3,6 +3,7 @@ package com.gamersafer.minecraft.abbacaving.commands;
 import com.gamersafer.minecraft.abbacaving.AbbaCavingPlugin;
 import com.gamersafer.minecraft.abbacaving.lobby.LobbyQueue;
 import java.util.Map;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,32 +20,45 @@ public class JoinCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command, @NotNull final String label, @NotNull final String[] args) {
-        if (!(sender instanceof Player player)) {
-            return true; // TODO: allow console/admins to force players into games
-        }
-
         if (args.length == 0) {
-            this.plugin.message(player, this.plugin.configMessage("join-invalid-map"));
+            this.plugin.message(sender, this.plugin.configMessage("join-invalid-map"));
             return false;
         }
 
         final String input = args[0];
 
         if (this.plugin.mapSettings(input) == null) {
-            this.plugin.message(player, this.plugin.configMessage("join-invalid-map"));
+            this.plugin.message(sender, this.plugin.configMessage("join-invalid-map"));
             return false;
         }
 
         final LobbyQueue queue = this.plugin.lobby().lobbyQueue(input);
 
+        final Player player;
+
+        if (args.length == 2) {
+            player = Bukkit.getPlayer(args[1]);
+
+            if (player == null) {
+                this.plugin.message(sender, this.plugin.configMessage("not-online"), Map.of("player", args[1]));
+                return true;
+            }
+        } else {
+            if (sender instanceof Player playerSender) {
+                player = playerSender;
+            } else {
+                return true;
+            }
+        }
+
         if (!queue.acceptingNewPlayers() && !player.hasPermission("abbacaving.join.full")) {
-            this.plugin.message(player, this.plugin.configMessage("join-full"));
+            this.plugin.message(sender, this.plugin.configMessage("join-full"));
             return false;
         }
 
         queue.addPlayer(player.getUniqueId());
 
-        this.plugin.message(player, this.plugin.configMessage("join-lobby"), Map.of("map", input));
+        this.plugin.message(sender, this.plugin.configMessage("join-lobby"), Map.of("map", input));
 
         return true;
     }
